ab3b7b24a4b9a57e152d7b811429d473812ab04e
ALowtonQA <alowton@qa.com>
2023-04-12 14:41:16 +0100

    AL: updated react proj

--- blogs - Copy/src/App.js
+++ blogs - Copy/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import allBlogs from './blogData.json';
 import BlogList from './components/BlogList';
+import { useState } from 'react';
 
 function App() {
+
+  const [blogs, setBlogs] = useState(allBlogs);
+
+  function deleteBlog(id) {
+    const filtered = blogs.filter(item => item.id !== id);
+    setBlogs(filtered);
+  }
+
   return ( 
     <div>
-      <BlogList blogs={allBlogs}/>
+      <BlogList blogs={blogs} deleteBlog={deleteBlog}/>
     </div>
   );
 
