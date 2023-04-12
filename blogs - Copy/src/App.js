dc3ea86ad0cf563407235833cab9d464154e969f
ALowtonQA <alowton@qa.com>
2023-04-12 12:33:31 +0100

    AL: added blogs proj

---
+++ blogs - Copy/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import allBlogs from './blogData.json';
+
+function App() {
+
+  const blogList = allBlogs.map(blog => {
+    return (
+      <div key={blog.id}>
+        <div>Title:{blog.title}</div>
+        <div>Author: {blog.author}</div>
+        <div>Body: {blog.body}</div>
+      </div>
+    )
+  });
+  return blogList;
+}
+
+export default App;
+
