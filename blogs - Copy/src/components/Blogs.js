26aadb04aac01a1ad3421c1a457fbeb0800d5032
ALowtonQA <alowton@qa.com>
2023-04-13 10:47:00 +0100

    AL: updated react proj

--- blogs - Copy/src/components/Blogs.js
+++ blogs - Copy/src/components/Blogs.js
@@ -1,15 +1,28 @@
 import BlogList from './BlogList';
 import { useEffect, useState } from 'react';
+import Spinner from './Spinner';
 
 function Blogs() {
     const [blogs, setBlogs] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch("http://localhost:4000/blogs")
             .then((res) => {
+
+                if (!res.ok) {
+                    throw new Error("Could not get data for that resource.");
+                }
+
                 return res.json();
             }).then((data) => {
                 setBlogs(data);
+                setError(null);
+                setIsLoading(false);
+            }).catch((err) => {
+                setError(err.message);
+                setIsLoading(false);
             });
     }, []);
 
@@ -20,6 +33,8 @@ function Blogs() {
 
     return (
         <div>
+            {error && <div> {error} </div>}
+            {isLoading && <Spinner />}
             {blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog} />}
         </div>
     );
