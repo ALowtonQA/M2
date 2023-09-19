6e5b281609c00ac97ac978d91516c7a85a83c78f
ALowtonQA <alowton@qa.com>
2023-09-19 15:07:37 +0100

    AL: working filter w/ usestate

--- Demo/blogs/src/App.js
+++ Demo/blogs/src/App.js
@@ -1,9 +1,17 @@
 import allBlogs from "./json/blogData.json";
 import BlogList from "./components/BlogList.js";
+import { useState } from "react";
 
 function App() {
+  const [blogs, setBlogs] = useState(allBlogs);
+
+  function deleteBlog(id) {
+    const newBlogs = blogs.filter(blog => blog.id != id);
+    setBlogs(newBlogs);
+  }
+
   return (
-    <BlogList blogs={allBlogs}/>
+    <BlogList blogs={blogs} deleteBlog={deleteBlog}/>
   )
 }
 
